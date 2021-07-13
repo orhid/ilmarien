73f801cc44d8777dc60772130953e3dd4e039241
Filip Oskar Przybycien <fprzybycien@gmail.com>
2021-07-13 15:53:45 +0200

    elevation generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ilmarien::geology as glg;
+//use ilmarien::imaging::cartography as crt;
+use ilmarien::imaging::colour as clr;
+
 fn main() {
-    println!("Hello, world!");
+    for seed in 0..1 {
+        let elevation = glg::elevation_generate(3456 + seed, seed);
+        //let elevation = crt::load("elevation-432".to_string());
+        //elevation.render(clr::HueInk::new(0.5, 0.0));
+        elevation.save();
+        //elevation.render(clr::ElevationInk);
+    }
 }
